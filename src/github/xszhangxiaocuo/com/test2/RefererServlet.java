4cf487b3c3307d44cabbf7f73d8457567df26f45
xszxc <2112067692@qq.com>
2023-09-17 16:42:36 +0800

    根据Referer字段判断有无盗链行为

---
+++ src/github/xszhangxiaocuo/com/test2/RefererServlet.java
@@ -0,0 +1,31 @@
+package github.xszhangxiaocuo.com.test2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "RefererServlet", value = "/test2/RefererServlet")
+public class RefererServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String referer = request.getHeader("Referer");
+        // 检查Referer是否为空或不包含本网站首页
+        if (referer == null || !referer.contains("/javaweb_war_exploded")) {
+            // 重定向回首页或其他适当的页面
+            response.sendRedirect("/javaweb_war_exploded/login.html");
+        } else {
+            // 正常显示页面内容
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("没有盗链行为，显示正常！");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
