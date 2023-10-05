dbc0ed9de40742cf2c0c73e95ee9dddc4025e55f
xszxc <2112067692@qq.com>
2023-10-06 00:51:19 +0800

    引入css文件，引入重复组件，遍历集合

---
+++ src/github/xszhangxiaocuo/com/test4/Book.java
@@ -0,0 +1,13 @@
+package github.xszhangxiaocuo.com.test4;
+
+public class Book {
+    public String bookid;
+    public String bookname;
+    public String publisher;
+
+    public Book(String bookid, String bookname, String publisher){
+        this.bookid = bookid;
+        this.bookname = bookname;
+        this.publisher = publisher;
+    }
+}
--- src/github/xszhangxiaocuo/com/test4/ToShowServlet.java
+++ src/github/xszhangxiaocuo/com/test4/ToShowServlet.java
@@ -8,27 +8,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-@WebServlet(name = "thymeleafTest1", value = "/test4/thymeleafTest1")
-public class thymeleafTest1 extends HttpServlet {
+@WebServlet(name = "ToShowServlet", value = "/test4/ToShowServlet")
+public class ToShowServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("name","重理工");
-        request.setAttribute("age","32");
-
         response.setContentType("text/html;charset=utf-8");
-        //创建模版引擎
+
+        Book book1 = new Book("1","书本1","出版社1");
+        Book book2 = new Book("2","书本2","出版社2");
+        Book book3 = new Book("3","书本3","出版社3");
+
+        List<Book> book = new ArrayList<>();
+        book.add(book1);
+        book.add(book2);
+        book.add(book3);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("book",book);
+
         WebApplication webApplication = new WebApplication(this.getServletContext());
         TemplateEngine templateEngine = webApplication.getTemplateEngine();
-        //获取上下文
         WebContext ctx = new WebContext(request,response,this.getServletContext());
-        //渲染页面
-        templateEngine.process("index1",ctx,response.getWriter());
+        templateEngine.process("book",ctx,response.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 }
